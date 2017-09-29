25cdf80a88a91e4ddb1e29b7f9427e631059e912
anderson rostirolla <anderson rostirolla>
2017-09-28 23:00:19 -0300

    Algoritmo tratando anomalia baseado no algoritmo de vizinhos mais proximos

---
+++ MaxMin/Classes/AlgoritmMaxMin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxMin
+{
+    public class AlgoritmMaxMin
+    {
+        private string anomalia;
+        private string valorFinal;
+        private double max, min;
+        private int visinhos;
+
+        public void setMaxMin(double min, double max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public double getMax()
+        {
+            return this.max;
+        }
+
+        public double getMin()
+        {
+            return this.min;
+        }
+
+        public void setClassificacao(Double valor)
+        {
+            if (valor > this.max || valor < this.min)
+            {
+                this.anomalia = "1";
+            }
+            else
+            {
+                this.anomalia = "0";
+            }
+        }
+
+        public void setVisinhos(int valor)
+        {
+            this.visinhos = valor;
+        }
+
+        public int getVisinhos()
+        {
+            return this.visinhos;
+        }
+
+        public String getClassificacao()
+        {
+            return anomalia;
+        }
+
+        public void trataAnomalia(List<String> listaValores)
+        {
+            string anomaliaClassificada = "";
+
+            /*foreach (double row in listaValores)
+            {
+                setClassificacao(row);
+                anomaliaClassificada = getClassificacao();
+
+                if (anomaliaClassificada == "1")
+                {
+                    if (Convert.ToDouble(row) == 0)
+                    {
+                        this.valorFinal = Convert.ToString(this.min);
+                    }
+                    else
+                    {
+                        this.valorFinal = Convert.ToString(Convert.ToDouble(row) / 2);
+                    }
+                }
+                else
+                {
+                    this.valorFinal = row;
+                }
+            }*/
+        }
+
+        public String valorTratado()
+        {
+            return valorFinal;
+        }
+    }
+}
---
+++ MaxMin/MaxMinTest.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaxMin
+{
+    public partial class MaxMinTest : Form
+    {
+        public MaxMinTest()
+        {
+            InitializeComponent();
+        }
+
+        private List<Double> linhaseparada = new List<Double>();
+        private List<String> classificacao = new List<String>();
+        private int cont = 0, cont2 = 0;
+        private AlgoritmMaxMin alg = new AlgoritmMaxMin();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            alg.setMaxMin(2.1,3.0);
+            alg.setVisinhos(2);
+            try
+            {
+                StreamReader rd = new StreamReader(@"C:\Users\Anderson\Desktop\file2.csv");
+                string linha = "";
+                while ((linha = rd.ReadLine()) != null)
+                {
+                    linhaseparada.Add(Convert.ToDouble(linha));
+                }
+
+                rd.Close();
+
+                recursiveMethod();
+
+                for (int i = 0; i < linhaseparada.Count(); i++)
+                {
+                    foreach (double row in linhaseparada)
+                    {
+                        alg.setClassificacao(row);
+                        classificacao.Add(alg.getClassificacao());
+                    }
+
+                    if (classificacao.Contains("1"))
+                    {
+                        recursiveMethod();
+                        classificacao.Clear();
+                    }
+                }
+
+                Console.WriteLine("Lista refeita:");
+                foreach (double row in linhaseparada)
+                {
+                    Console.WriteLine(row.ToString());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Erro ao executar Leitura do Arquivo");
+            }
+
+        }
+
+        public void recursiveMethod()
+        {
+            for (int i = 0; i <= linhaseparada.Count() - alg.getVisinhos(); i++)
+            {
+                if (i >= (linhaseparada.Count() - alg.getVisinhos()))
+                {
+                    for (int j = i; j < linhaseparada.Count(); j++)
+                    {
+                        cont = linhaseparada.Count() - j;
+                        if ((linhaseparada[j] >= alg.getMin()) && (linhaseparada[j] <= alg.getMax()))
+                        {
+                            linhaseparada[j] = linhaseparada[i];
+                        }
+                        else
+                        {
+                            if (j == linhaseparada.Count() - cont && alg.getVisinhos() == 1 && linhaseparada.Count() >= 1)
+                            {
+                                linhaseparada[j] = linhaseparada[j - 1];
+                            }
+                            else if (j == linhaseparada.Count() - cont && alg.getVisinhos() == 2 && linhaseparada.Count() >= 2)
+                            {
+                                linhaseparada[j] = ((linhaseparada[j - 1] + linhaseparada[j - 2]) / alg.getVisinhos());
+                            }
+                            else if (j == linhaseparada.Count() - cont && alg.getVisinhos() == 3 && linhaseparada.Count() >= 3)
+                            {
+                                linhaseparada[j] = ((linhaseparada[j - 1] + linhaseparada[j - 2] + linhaseparada[j - 3]) / alg.getVisinhos());
+                            }
+                        }
+                    }
+                }
+                else if (i >= alg.getVisinhos())
+                {
+                    if ((linhaseparada[i] >= alg.getMin()) && (linhaseparada[i] <= alg.getMax()))
+                    {
+                        linhaseparada[i] = linhaseparada[i];
+                    }
+                    else
+                    {
+                        switch (alg.getVisinhos())
+                        {
+                            case 1:
+                                linhaseparada[i] = ((linhaseparada[i + 1] + linhaseparada[i - 1]) / (alg.getVisinhos() * 2));
+                                break;
+                            case 2:
+                                linhaseparada[i] = ((linhaseparada[i - 2] + linhaseparada[i - 1] + linhaseparada[i + 1] + linhaseparada[i + 2]) / (alg.getVisinhos() * 2));
+                                break;
+                            case 3:
+                                linhaseparada[i] = ((linhaseparada[i - 3] + linhaseparada[i - 2] + linhaseparada[i - 1] + linhaseparada[i + 1] + linhaseparada[i + 2] + linhaseparada[i + 3]) / (alg.getVisinhos() * 2));
+                                break;
+                            default:
+                                Console.WriteLine("O Tratamento para visinhos foi implementado somente até 3.");
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    cont2 = ((i+alg.getVisinhos()) - alg.getVisinhos());
+                    if ((linhaseparada[i] >= alg.getMin()) && (linhaseparada[i] <= alg.getMax()))
+                    {
+                        linhaseparada[i] = linhaseparada[i];
+                    }
+                    else
+                    {
+                        if (i == cont2 && alg.getVisinhos() == 1 && linhaseparada.Count() >= 1)
+                        {
+                            if (linhaseparada[i + 1] >= alg.getMin())
+                                linhaseparada[i] = linhaseparada[i + 1];
+                            else
+                                linhaseparada[i] = alg.getMin();
+                        }
+                        else if (i == cont2 && alg.getVisinhos() == 2 && linhaseparada.Count() >= 2)
+                        {
+                            if (linhaseparada[i + 1] >= alg.getMin())
+                                linhaseparada[i] = ((linhaseparada[i + 1] + linhaseparada[i + 2]) / alg.getVisinhos());
+                            else
+                                linhaseparada[i] = alg.getMin();
+                        }
+                        else if (i == cont2 && alg.getVisinhos() == 3 && linhaseparada.Count() >= 3)
+                        {
+                            if (linhaseparada[i + 1] >= alg.getMin())
+                                linhaseparada[i] = ((linhaseparada[i + 1] + linhaseparada[i + 2] + linhaseparada[i + 3]) / alg.getVisinhos());
+                            else
+                                linhaseparada[i] = alg.getMin();
+                        }
+                    }
+                }
+            }
+
+
+        }
+    }
+}
